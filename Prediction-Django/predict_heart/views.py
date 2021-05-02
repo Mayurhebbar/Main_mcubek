7d850c22c998c1bc9bd95a0edac9bc285e4e2783
MeghanaMahesh <meghanamahesh666@gmail.com>
2021-05-02 19:06:36 +0530

    heart disease

--- Prediction-Django/predict_heart/views.py
+++ Prediction-Django/predict_heart/views.py
@@ -6,23 +6,22 @@ from .models import PredResults
 from sklearn.preprocessing import StandardScaler
 
 
-def predict(request):
-    return render(request, 'predict.html')
+def predict_heart(request):
+    return render(request, 'predict_heart.html')
 
 
-def predict_chances(request):
+def predict_chances_heart(request):
     if request.POST.get('action') == 'post':
         # Receive data from client
         Patient_ID = int(request.POST.get('Patient_ID'))
-        print("PID", Patient_ID)
+
         Patient_Age = int(request.POST.get('Patient_Age'))
-        print("Page", Patient_Age)
+
         Patient_Gender = int(request.POST.get('Patient_Gender'))
-        print("Pgen", Patient_Gender)
+
         CP = int(request.POST.get('cp'))
-        print("CP = ", CP)
+
         Trestbps = int(request.POST.get('trestbps'))
-        print("trestbps = ", Trestbps)
         Cholesterol = int(request.POST.get('chol'))
         #FBS = int(request.POST.get('fbs'))
         Restecg = int(request.POST.get('restecg'))
@@ -51,12 +50,10 @@ def predict_chances(request):
 
         # Unpickle model
         model = joblib.load("heart_model")
-        # Make prediction
         result = model.predict([[Patient_Age1[0][0], Patient_Gender1[0][0], CP[0][0], Trestbps[0][0], Cholesterol[0][0], Restecg[0][0],
                                  Thalach[0][0], Exang[0][0], Oldpeak[0][0], Slope[0][0], Thal[0][0]]])
 
         Heart_Disease = result[0]
-        print("heart..................",Heart_Disease)
 
         PredResults.objects.create(Patient_ID=Patient_ID, Patient_Age=Patient_Age, Patient_Gender=Patient_Gender,
                                    Heart_Disease=Heart_Disease)
@@ -65,7 +62,7 @@ def predict_chances(request):
                             safe=False)
 
 
-def view_results(request):
+def view_results_heart(request):
     # Submit prediction and show all
     data = {"dataset": PredResults.objects.all()}
-    return render(request, "results.html", data)
+    return render(request, "results_heart.html", data)
