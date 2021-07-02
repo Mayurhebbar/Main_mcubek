7fc766e5f5f301ec15d0007bf5a44976624da41f
MeghanaMahesh <meghanamahesh666@gmail.com>
2021-07-02 17:33:08 +0530

    added consulted_doctor field

--- Prediction-Django/predict_cancers/models.py
+++ Prediction-Django/predict_cancers/models.py
@@ -6,6 +6,7 @@ class PredResults_cancers(models.Model):
     Patient_ID = models.IntegerField()
     Patient_Age = models.IntegerField()
     Patient_Gender = models.IntegerField()
+    consulted_doctor = models.IntegerField()
     Cancer_Disease = models.CharField(max_length=30)
 
     def __str__(self):
--- Prediction-Django/predict_diabetes/models.py
+++ Prediction-Django/predict_diabetes/models.py
@@ -6,6 +6,7 @@ class PredResults_diabetes(models.Model):
     Patient_ID = models.IntegerField()
     Patient_Age = models.IntegerField()
     Patient_Gender = models.IntegerField()
+    consulted_doctor = models.IntegerField()
     Diabetes_Disease = models.CharField(max_length=30)
 
     def __str__(self):
